ebb17e239c9f2cfd974ff9617b3fb1c3f8ec67e7
milochase13 <milochase13@gmail.com>
2024-09-08 23:27:41 -0700

    fix bug

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -9,12 +9,7 @@ import { useEffect, useState } from "react";
  */
 export default function Blog() {
     const [posts, setPosts] = useState([]);
-    // const [renderedPosts, setRenderedPosts] = useState([]);
     const contentPath = "/BlogPosts/posts.json"
-    // const postFiles = [
-    //     {"7": "1 June 2024"},
-    //     {"Media Provenance": "7 September 2024"}
-    // ]
 
     const fetchPostsData = async () => {
         const response = await fetch(contentPath);
@@ -28,7 +23,6 @@ export default function Blog() {
 
     return ( 
       <div className="blog">
-        {console.log("TEST2", posts)}
         <SectionHeader text="Blog" alignment="Center"/>
         <hr className="divider"/>
         {posts.toReversed().map((p) => <BlogPost key={p["title"]} date={p["date"]} title={p["title"]} body={p["md_content"]}/>)}
